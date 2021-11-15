e68cd5088de3aa59bfbe67e3f05d0ef992115a61
vovanbart <vovanbart@icloud.com>
2021-11-15 22:25:48 +0300

    8

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = ''
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
