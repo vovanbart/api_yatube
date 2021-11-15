079b9c356d1ab995a4d67479e029113af6b17b42
vovanbart <vovanbart@icloud.com>
2021-11-15 22:52:16 +0300

    8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,7 +8,7 @@ class AuthorPermission(permissions.BasePermission):
             return True
         return False
 
-    def has_object_permission(self, request, obj):
+    def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.author == request.user
